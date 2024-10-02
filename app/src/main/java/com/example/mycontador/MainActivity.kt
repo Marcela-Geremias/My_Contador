9af7a870c05c37812c4da71dba601bc5eb7f9cf7
Marcela <marcelarochageremias@gmail.com>
2024-10-01 22:00:19 -0300

    v2

--- app/src/main/java/com/example/mycontador/MainActivity.kt
+++ app/src/main/java/com/example/mycontador/MainActivity.kt
@@ -1,12 +1,15 @@
 package com.example.mycontador
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    var contador = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +19,17 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val textContador = findViewById<TextView>(R.id.textView)
+        val btn_aumentar = findViewById<Button>(R.id.activity_main_btn_aumentar)
+        val btn_diminuir = findViewById<Button>(R.id.activity_main_btn_diminuir)
+
+        btn_aumentar.setOnClickListener {
+        textContador.text = "${++contador}"
+    }
+        btn_diminuir.setOnClickListener {
+            textContador.text = "${--contador}"
+        }
+
     }
 }
